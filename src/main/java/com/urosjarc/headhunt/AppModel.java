a1e4bc916beb84d1771a01eb7c338b1810e0e3d0
urosjarc <urosjarc@users.noreply.github.com>
2015-12-05 12:10:37 +0100

    * Deleting schema tables. * Setting very basic fxml root structure. * Replacing orml with raw sql wraper.

--- src/main/java/com/urosjarc/headhunt/AppModel.java
+++ src/main/java/com/urosjarc/headhunt/AppModel.java
@@ -1,22 +1,17 @@
 package com.urosjarc.headhunt;
 
-import com.urosjarc.headhunt.schema.Notifier;
-import com.urosjarc.headhunt.schema.Project;
-import com.urosjarc.headhunt.schema.Task;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import com.j256.ormlite.dao.DaoManager;
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
-import com.j256.ormlite.support.ConnectionSource;
-import com.j256.ormlite.table.TableUtils;
 import javax.annotation.PostConstruct;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class AppModel {
 
-    private static ConnectionSource connectionSource;
+    private static Connection dbConnection;
 
     @PostConstruct
     public void init() throws SQLException {
@@ -31,32 +26,10 @@ public class AppModel {
     }
 
     public static void openDB(String databaseUrl) throws SQLException {
-        connectionSource = new JdbcConnectionSource(databaseUrl);
-
-        //This should be injected!
-        TableUtils.createTable(connectionSource, Task.class);
-        Task.table = DaoManager.createDao(connectionSource, Task.class);
-
-        TableUtils.createTable(connectionSource, Project.class);
-        Project.table = DaoManager.createDao(connectionSource, Project.class);
-
-        TableUtils.createTable(connectionSource, Notifier.class);
-        Notifier.table = DaoManager.createDao(connectionSource, Notifier.class);
-        //injected-end
-
-        //Migration
-        Project project = new Project();
-        Project project1 = new Project();
-        project.setName("Sample project");
-        project1.setName("New project");
-
-        Project.table.create(project);
-        Project.table.create(project1);
-        //migration-end
-
+        dbConnection = DriverManager.getConnection(databaseUrl);
     }
 
     public static void closeDB() throws SQLException {
-        connectionSource.close();
+        dbConnection.close();
     }
 }
