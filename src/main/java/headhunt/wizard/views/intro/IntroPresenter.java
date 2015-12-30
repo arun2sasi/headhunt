b865b711d4f899431fab7ca5691712cd2c977ebf
urosjarc <urosjarc@users.noreply.github.com>
2015-12-31 00:03:00 +0100

    Finishing intro

--- src/main/java/headhunt/wizard/views/intro/IntroPresenter.java
+++ src/main/java/headhunt/wizard/views/intro/IntroPresenter.java
@@ -1,25 +1,11 @@
 package headhunt.wizard.views.intro;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.text.TextFlow;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class IntroPresenter implements Initializable {
-
-    @FXML
-    private ImageView setupImage;
-
-    @FXML
-    private TextFlow header;
-
-    @FXML
-    private TextFlow info;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
