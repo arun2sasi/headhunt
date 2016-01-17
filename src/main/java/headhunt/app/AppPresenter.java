7dd200a043dd9a1a8745a67b456b82deade98929
urosjarc <urosjarc@users.noreply.github.com>
2016-01-17 12:06:22 +0100

    Small fixes on app menu bar.

--- src/main/java/headhunt/app/AppPresenter.java
+++ src/main/java/headhunt/app/AppPresenter.java
@@ -63,67 +63,6 @@ public class AppPresenter implements Initializable {
 		System.out.println("Show result");
 	}
 
-//    public void importTwitterUsers(){
-//
-//        FileChooser fileChooser = new FileChooser();
-//        fileChooser.setTitle("Open Resource File");
-//        fileChooser.getExtensionFilters().addAll(
-//                new FileChooser.ExtensionFilter("*.json", "*.json"),
-//                new FileChooser.ExtensionFilter("All", "*.*"));
-//        File file = fileChooser.showOpenDialog(new Stage());
-//
-//        JSONParser parser = new JSONParser();
-//        try {
-//            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(file));
-//
-//            LoadDialogView loadingDialog = new LoadDialogView("Loading...");
-//
-//            loadingDialog.setTask(
-//                "Importing twitter users: " + jsonArray.size() + " users...",
-//                new Task<Void>() {
-//                    private int index = 0;
-//                    private final int size = jsonArray.size();
-//
-//                    @Override
-//                    protected Void call() throws Exception {
-//                        ODatabaseRecordThreadLocal.INSTANCE.set(AppModel.getDb().getUnderlying());
-//
-//                        index = 0;
-//
-//                        jsonArray.forEach(new Consumer() {
-//                            @Override
-//                            public void accept(Object object) {
-//
-//                                if (isCancelled()) {
-//                                    return;
-//                                }
-//
-//                                VimeoUser.insertOrUpdate(object);
-//                                updateProgress((double) index,size);
-//                                index++;
-//                                updateMessage("Importing twitter users: " + index + "/" + size);
-//                            }
-//                        });
-//
-//                        updateMessage("Importing twitter users: " + index + "/" + size + " SUCCESS");
-//                        resultsTable.setItems(FXCollections.observableArrayList(VimeoUser.getAll()));
-//
-//                        return null;
-//                    }
-//            });
-//
-//
-//
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-
     public void exit(){
         Platform.exit();
     }
